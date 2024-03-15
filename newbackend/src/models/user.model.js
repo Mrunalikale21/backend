96fb9793dc854ae477343c63d1b706e635c93551
Mrunalikale21 <mrunalikale76@gmail.com>
2024-03-15 22:20:29 +0530

    commit

---
+++ newbackend/src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index:true  //for optimal search
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url
+    },
+    watchHistory: [
+      {
+       type: Schema.Types.ObjectId,
+       ref: "Video"
+    }
+  ],
+  password: {
+      type: String,
+      required: [true, 'Password is required']
+  },
+  refreshToken: { //long string
+    type:String
+  }
+},
+{
+  timestamps: true
+}
+)
+
+userSchema.pre("save", async function(next){
+  if(!this.isMondified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10)
+  next()
+}) //next for middleware , callback is written differnt coz we want this reference
+
+userSchema.methods.isPasswordCorrect = async function(password){
+ return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName : this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generatefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema)
