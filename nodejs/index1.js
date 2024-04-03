957605bf92d56836844df0475b32474f47a9f7bc
Mrunalikale21 <mrunalikale76@gmail.com>
2024-04-03 19:47:38 +0530

    commit

--- nodejs/index1.js
+++ nodejs/index1.js
@@ -1,6 +1,16 @@
 import  express  from "express";
+import path from path
 
 const app = express();
+
+app.get("/" , (req,res) => {
+  // res.json({
+  //   success : true,
+  //   product : []
+  // })
+  const pathLocation = path.resolve();
+  res.sendFile(path.join(pathLocation, "./index.html"))
+})
 
 app.listen(5000, () => {
   console.log("server is working")
